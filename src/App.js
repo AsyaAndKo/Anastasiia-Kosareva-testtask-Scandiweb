a17eb5b7ce8c83f06d735ee5150043a9e8bfc6ce
AsyaAndKo <asya.kosareva666@gmail.com>
2022-03-11 17:56:33 +0200

    Add routing to navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header/Header";
 
 export default class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <div className={App}>
+        <Header />
+      </div>
+    );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
-import Navbar from "../Navbar/Navbar";
+import {
+  LeftContainer,
+  CenterContainer,
+  RightContainer,
+  HeaderContainer,
+} from "../styles/Header.style";
+import Navbar from "./Navbar/Navbar";
+import Clothes from "../Content/Clothes/Clothes";
+import Tech from "../Content/Tech/Tech";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { from } from "apollo-boost";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <>
+        <BrowserRouter>
+          <HeaderContainer>
+            <LeftContainer>
+              <Navbar />
+            </LeftContainer>
+            <CenterContainer>
+              <img src="../../assets/shop-logo.svg" alt="logo" />
+            </CenterContainer>
+            <RightContainer>
+              <></>
+            </RightContainer>
+            <Routes>
+              <Route path="/" />
+              <Route path="/clothes" element={<Clothes />} />
+              <Route path="/tech" element={<Tech />} />
+            </Routes>
+          </HeaderContainer>
+        </BrowserRouter>
+      </>
     );
   }
 }
