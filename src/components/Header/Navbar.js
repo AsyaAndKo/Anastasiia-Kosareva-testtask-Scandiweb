a125807384fd7e95ea8eb6f612c1bda4a3829a1c
AsyaAndKo <asya.kosareva666@gmail.com>
2022-03-20 12:33:17 +0200

    Add Cart file and remove Navbar to Header folder

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {
+  NavbarContainer,
+  NavbarLink,
+  NavbarLinkContainer,
+} from "../styles/Navbar.style";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <NavbarContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/all">All</NavbarLink>
+            <NavbarLink to="/clothes">Clothes</NavbarLink>
+            <NavbarLink to="/tech">Tech</NavbarLink>
+          </NavbarLinkContainer>
+        </NavbarContainer>
+      </>
+    );
+  }
+}
