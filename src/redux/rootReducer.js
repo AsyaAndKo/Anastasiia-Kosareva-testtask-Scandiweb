56a7bfd118e75b179b376ad926300217b9cba851
AsyaAndKo <asya.kosareva666@gmail.com>
2022-04-08 20:03:00 +0300

    Add Product ID reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import currencyReducer from "./Currency/currency.reducer";
 import categoryReducer from "./Category/category.reducer";
+import productIDReducer from "./ProductID/productID.reducer";
 
 export default combineReducers({
   currentCurrency: currencyReducer,
   currentCategory: categoryReducer,
+  currentProductID: productIDReducer,
 });
