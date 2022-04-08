f68dbbb746d63316e6248402d25b4df77b3e7ac8
AsyaAndKo <asya.kosareva666@gmail.com>
2022-04-08 12:38:38 +0300

    Implement the redux provider into app

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 export const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Provider>,
   document.getElementById("root")
 );
