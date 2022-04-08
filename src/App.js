e2e171069612d94d41b09e5afbdeba2495375561
AsyaAndKo <asya.kosareva666@gmail.com>
2022-04-08 20:28:05 +0300

    Remove redundant imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { connect } from "react-redux";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Cart from "./components/Content/Cart/Cart";
 import Content from "./components/Content/Content";
 import ProductPage from "./components/Content/ProductPage";
-
-// import { setCurrentCurrency } from "./redux/Currency/currency.actions";
 
 class App extends Component {
   render() {
