7c5179d296c83b902a98e25dbb046bdba2e73544
AsyaAndKo <asya.kosareva666@gmail.com>
2022-04-11 17:14:57 +0300

    Add cartIsOpen reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import currencyReducer from "./Currency/currency.reducer";
 import categoryReducer from "./Category/category.reducer";
 import productIDReducer from "./ProductID/productID.reducer";
+import cartIsOpenReducer from "./CartOpen/cartOpen.reducer";
 
 export default combineReducers({
   currentCurrency: currencyReducer,
   currentCategory: categoryReducer,
   currentProductID: productIDReducer,
+  cartIsOpen: cartIsOpenReducer,
 });
