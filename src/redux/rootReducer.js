2c26013a3176d43d73a8e3fd060680de4017ca42
AsyaAndKo <asya.kosareva666@gmail.com>
2022-04-13 21:20:44 +0300

    Add cart reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,10 +4,12 @@ import currencyReducer from "./Currency/currency.reducer";
 import categoryReducer from "./Category/category.reducer";
 import productIDReducer from "./ProductID/productID.reducer";
 import cartIsOpenReducer from "./CartOpen/cartOpen.reducer";
+import { cartReducer } from "./Cart/cart.reducer";
 
 export default combineReducers({
   currentCurrency: currencyReducer,
   currentCategory: categoryReducer,
   currentProductID: productIDReducer,
   cartIsOpen: cartIsOpenReducer,
+  cartData: cartReducer,
 });
