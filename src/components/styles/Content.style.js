cb7da9c4b42d001955bce4eee7f9369d8f3d2752
AsyaAndKo <asya.kosareva666@gmail.com>
2022-05-23 10:08:46 +0300

    Remove fixed position

--- src/components/styles/Content.style.js
+++ src/components/styles/Content.style.js
@@ -7,7 +7,6 @@ export const ContentPage = styled.div`
   ${(props) =>
     props.cartIsOpen === "open"
       ? `
-    position: fixed;
     opacity: 50%;
     background-color: rgba(57, 55, 72, 0.22);
     pointer-events:none;
